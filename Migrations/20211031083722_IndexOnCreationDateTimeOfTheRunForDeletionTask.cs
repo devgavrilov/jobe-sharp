a4166b590518c8bd2e224502122f176c3b57aad2
Ilya Gavrilov <dev.gavrilov@gmail.com>
2021-10-31 11:40:43 +0300

    Missing migration.

---
+++ Migrations/20211031083722_IndexOnCreationDateTimeOfTheRunForDeletionTask.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JobeSharp.Migrations
+{
+    public partial class IndexOnCreationDateTimeOfTheRunForDeletionTask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CreationDateTimeUtc",
+                table: "Runs",
+                column: "CreationDateTimeUtc");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CreationDateTimeUtc",
+                table: "Runs");
+        }
+    }
+}
